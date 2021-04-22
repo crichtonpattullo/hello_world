5a7914ea476be1d679b368dd99cbdbc9335dba0c
Crichton <crichton.pattullo@pcc.edu>
2021-04-21 18:11:31 -0700

    Added main method that prints hello world

--- src/edu/pcc/cis/hello_world/HelloWorld.java
+++ src/edu/pcc/cis/hello_world/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.hello_world;
 
+/**
+ * Author: Crichton Pattullo
+ * Version: Week 4
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
